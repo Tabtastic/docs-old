696d8e068d7132e99006c38f2e8e95162c1ca955
Sébastien Dunne Fulmer <createdbyseb@gmail.com>
2023-05-28 08:07:30 +0100

    Bootstrap Vitepress project

---
+++ .vitepress/config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Tabtastic Docs",
+  description: "Documentation for Tabtastic, a platform for organising debating competitions",
+  lang: "en-GB",
+
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    logo: {
+      light: "/wordmark-light.png",
+      dark: "/wordmark-dark.png",
+      alt: "Tabtastic Docs",
+    },
+
+    siteTitle: false,
+
+    nav: [
+      { text: 'Guides', link: '/guides/' },
+      { text: 'Features', link: '/features/' },
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Tabtastic/docs' },
+      { icon: 'discord', link: 'https://discord.gg/6UEYkQW' },
+      { icon: 'facebook', link: 'https://facebook.com/TabtasticApp' },
+    ],
+
+    footer: {
+      message: "Released under the GPL-3.0 License.",
+      copyright: "Copyright © 2023-present Sébastien Dunne Fulmer",
+    },
+
+    editLink: {
+      pattern: 'https://github.com/Tabtastic/docs/edit/main/:path',
+      text: 'Edit this page on GitHub',
+    },
+
+    search: {
+      provider: "local",
+    },
+  },
+})
---
+++ index.md
@@ -0,0 +1,30 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: Tabtastic Docs
+  tagline: Learn how to get the most out of Tabtastic
+  image:
+    src: /icon.png
+    alt: ""
+  actions:
+    - theme: brand
+      text: Set Up a Tournament
+      link: /guides/setup
+    - theme: alt
+      text: Running a Round
+      link: /guides/round
+
+features:
+  - title: Guides
+    details: Walkthroughs for common parts of setting up or running a tournament
+    link: /guides/
+  - title: Features
+    details: Learn how to configure and use different Tabtastic features
+    link: /features/
+  - title: Discord
+    details: Ask questions, report bugs or suggest new features in our Discord server
+    link: "https://discord.gg/6UEYkQW"
+---
+
